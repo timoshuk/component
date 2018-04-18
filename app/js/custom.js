1c347b7d673fcc802eea25e45eba1cdf6605e54e
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2018-04-18 16:26:30 +0300

    Finished html css & JS main-nav

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,1 +1,42 @@
-"use strict";
+'use strict';
+
+window.onload = function () {
+    /*=====================
+    main-nav
+    ======================*/
+    var navBtn = document.querySelector('.main-nav__btn');
+    var navBtnLine1 = document.querySelector('.main-nav__line:first-child');
+    var navBtnLine2 = document.querySelector('.main-nav__line:nth-child(2)');
+    var navBtnLine3 = document.querySelector('.main-nav__line:last-child');
+    var mainMenu = document.querySelector('.main-nav__lists');
+    var navLink = document.querySelectorAll('.main-nav__link');
+
+    // Show menu on click nav__btn
+    navBtn.addEventListener('click', function () {
+        navBtnLine1.classList.toggle('main-nav--line-1');
+        navBtnLine2.classList.toggle('main-nav--line-2');
+        navBtnLine3.classList.toggle('main-nav--line-3');
+        mainMenu.classList.toggle('show');
+    });
+
+    // Hide menu on click to button
+
+    for (var i = 0; i < navLink.length; i++) {
+        navLink[i].addEventListener('click', function (e) {
+            var targetElement = event.target || event.srcElement;
+            navBtnLine1.classList.toggle('main-nav--line-1');
+            navBtnLine2.classList.toggle('main-nav--line-2');
+            navBtnLine3.classList.toggle('main-nav--line-3');
+            mainMenu.classList.toggle('show');
+
+            if (targetElement.classList.contains('main-nav--active')) {
+                return;
+            } else {
+                for (var k = 0; k < navLink.length; k++) {
+                    navLink[k].classList.remove('main-nav--active');
+                }
+                targetElement.classList.add('main-nav--active');
+            }
+        });
+    }
+};
---
+++ app/scripts/custom.js
@@ -0,0 +1,42 @@
+window.onload = () => {
+    /*=====================
+    main-nav
+    ======================*/
+    let navBtn = document.querySelector('.main-nav__btn');
+    let navBtnLine1 = document.querySelector('.main-nav__line:first-child');
+    let navBtnLine2 = document.querySelector('.main-nav__line:nth-child(2)');
+    let navBtnLine3 = document.querySelector('.main-nav__line:last-child');
+    let mainMenu = document.querySelector('.main-nav__lists');
+    let navLink = document.querySelectorAll('.main-nav__link');
+
+    // Show menu on click nav__btn
+    navBtn.addEventListener('click', () => {
+        navBtnLine1.classList.toggle('main-nav--line-1');
+        navBtnLine2.classList.toggle('main-nav--line-2');
+        navBtnLine3.classList.toggle('main-nav--line-3');
+        mainMenu.classList.toggle('show');
+    });
+
+    // Hide menu on click to button
+
+    for (let i = 0; i < navLink.length; i++) {
+        navLink[i].addEventListener('click', (e) => {
+            let targetElement = event.target || event.srcElement;
+            navBtnLine1.classList.toggle('main-nav--line-1');
+            navBtnLine2.classList.toggle('main-nav--line-2');
+            navBtnLine3.classList.toggle('main-nav--line-3');
+            mainMenu.classList.toggle('show');
+            
+
+            if(targetElement.classList.contains('main-nav--active')){
+                return;
+            }else{
+                for (let k = 0; k < navLink.length; k++){
+                    navLink[k].classList.remove('main-nav--active');
+                }
+                targetElement.classList.add('main-nav--active');
+            }
+            
+        });
+    }
+};
